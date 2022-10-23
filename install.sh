2534090ee4b4f0cfa0ba4adaa5c171fcc46ed667
MALLU DUDE <104883312+MalluDude@users.noreply.github.com>
2022-10-23 21:43:13 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,85 @@
+#!/data/data/com.termux/files/usr/bash
+echo $KING
+echo ""----------------------------------------------------------------------------- ""
+echo " This TOOL is CREATED by MALLU DUDE"
+echo "---------------------------------------------------------------------------------"
+
+echo
+
+echo
+echo
+echo
+echo
+
+clear
+
+echo "...."
+echo "......"
+
+echo
+echo                 figlet MALLU DUDE
+echo
+
+apt update -y                      #update in progress
+apt install python -y           #installing...
+apt install python2 -y
+apt install ruby -y
+apt install git -y
+
+echo "......."
+echo "........."
+
+clear
+
+apt install php -y
+apt install perl -y
+apt install nmap -y
+
+echo "......."
+echo "........."
+
+clear
+
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+
+echo "......."
+echo "........."
+
+clear
+
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+
+echo "......."
+echo "........."
+
+clear
+
+apt install unzip -y
+apt install tor -y
+apt install wget -y
+
+apt install wcalc -y
+apt install openssl
+apt install bmon -y
+
+
+echo " THANK YOU FOR WAITING "
+echo " YOUR PACKAGES IS INSTALLED SUCCESSFULLY "
+
+clear
+
+echo
+echo               "..................................."
+echo                  " THANK YOU REDSPY TEAM "               
+echo                  " SUBSCRIBE AND SUPPORT "
+echo           " YT- https://youtube.com/c/REDSPYTECH "
+echo  " IG- https://www.instagram.com/redspy_tech?r=nametag " 
+echo               "...................................."
+echo
+echo
